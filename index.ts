06821b7b359fc5a1237bcffa485cc242407ae72a
Ankit6055 <ankit.kumarrrrr6055@gmail.com>
2025-09-08 17:32:05 +0530

    Tavily Search Tool Added

--- index.ts
+++ index.ts
@@ -2,6 +2,25 @@ import { stat } from "node:fs";
 import readline from "node:readline/promises";
 import { StateGraph, MessagesAnnotation } from "@langchain/langgraph";
 import { ChatGroq } from "@langchain/groq";
+import { ToolNode } from "@langchain/langgraph/prebuilt";
+import { TavilySearch } from "@langchain/tavily";
+
+const tool = new TavilySearch({
+  maxResults: 5,
+  topic: "general",
+  // includeAnswer: false,
+  // includeRawContent: false,
+  // includeImages: false,
+  // includeImageDescriptions: false,
+  // searchDepth: "basic",
+  // timeRange: "day",
+  // includeDomains: [],
+  // excludeDomains: [],
+});
+
+// Initialise the tool node
+const tools: any = [];
+const toolNode = new ToolNode(tools);
 
 /*
 1. Define node function
@@ -19,18 +38,28 @@ const llm = new ChatGroq({
 
 async function callModel(state: typeof MessagesAnnotation.State) {
   // call the LLMs using APIs
-  console.log("Calling LLM...")
+  console.log("Calling LLM...");
   const response = await llm.invoke(state.messages);
 
   // We return a list, because this will get added to the existing list
   return { messages: [response] };
 }
 
+function shouldContinue(state: any) {
+  // put your condition
+  // whether to call a tool or end
+
+  console.log("state", state);
+  return "__end__";
+}
+
 // Build the graph
 const workFlow = new StateGraph(MessagesAnnotation)
   .addNode("agent", callModel)
+  .addNode("tools", toolNode)
   .addEdge("__start__", "agent")
-  .addEdge("agent", "__end__");
+  .addEdge("agent", "__end__")
+  .addConditionalEdges("agent", shouldContinue);
 
 // Compile the graph
 const app = workFlow.compile();
@@ -45,7 +74,7 @@ async function main() {
     const userInput = await rl.question("You: ");
 
     if (userInput === "/bye") break;
-    
+
     // Invoke the graph
     const finalState = await app.invoke({
       messages: [{ role: "user", content: userInput }],
