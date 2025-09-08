e88946b5dfd9d6b7df3b7c8396367955fb0cc8cd
Ankit6055 <ankit.kumarrrrr6055@gmail.com>
2025-09-08 16:54:44 +0530

    langgraph config done

--- index.ts
+++ index.ts
@@ -1,4 +1,26 @@
+import { stat } from "node:fs";
 import readline from "node:readline/promises";
+import { StateGraph, MessagesAnnotation } from "@langchain/langgraph";
+
+/*
+1. Define node function
+2. Build the graph
+3. Compile and invoke the graph
+*/
+
+function callModel(state: typeof MessagesAnnotation.State) {
+  // call the LLMs using APIs
+  return state;
+}
+
+// Build the graph
+const workFlow = new StateGraph(MessagesAnnotation)
+  .addNode("agent", callModel)
+  .addEdge("__start__", "agent")
+  .addEdge("agent", "__end__");
+
+// Compile the graph
+const app = workFlow.compile();
 
 async function main() {
   const rl = readline.createInterface({
@@ -10,7 +32,13 @@ async function main() {
     const userInput = await rl.question("You: ");
 
     if (userInput === "/bye") break;
-    console.log("You said:", userInput);
+    
+    // Invoke the graph
+    const finalState = await app.invoke({
+      messages: [{ role: "user", content: userInput }],
+    });
+
+    console.log("Final: ", finalState);
   }
 
   rl.close();
