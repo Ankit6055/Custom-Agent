e55be042df5d801cc3b71517dc25f5fcaa40956e
Ankit6055 <ankit.kumarrrrr6055@gmail.com>
2025-09-08 16:14:58 +0530

    Setup done

--- index.ts
+++ index.ts
@@ -1,1 +1,15 @@
-console.log("Hello via Bun!");
+import readline from "node:readline/promises";
+
+async function main() {
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+
+  const userInput = await rl.question("You: ");
+  console.log("You said:",userInput);
+
+  rl.close();
+}
+
+main();
